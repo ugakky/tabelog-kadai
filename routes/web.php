7f23bc5ce950771ca793992ff36d8644705bcd5d
ugakky <mon_pen2@yahoo.co.jp>
2024-03-31 20:35:04 +0900

    deploy to heroku

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +20,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+Auth::routes(['verify' => true]);
+
+Route::get('/', [RestaurantController::class, 'index'])->name('restaurants.index');
+
+Route::get('/reviews', [ReviewController::class, 'index'])->name('reviews.index');
+Route::get('/reviews/register', [ReviewController::class, 'store'])->name('reviews.register');
+Route::post('/reviews/register', [ReviewController::class, 'store'])->name('reviews.register');
+
+Route::get('/restaurants/{restaurant}', [RestaurantController::class, 'detail'])->name('restaurants.detail');
+
+Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+Route::get('/reservations/subscription', [ReservationController::class, 'create'])->name('subscription');
+Route::post('/reservations/register', [ReservationController::class, 'store'])->name('reservations.register');
+Route::get('restaurants/{restaurant}/favorite', [RestaurantController::class, 'favorite'])->name('restaurants.favorite');
+Route::get('restaurants/{restaurant}/unfavorite', [RestaurantController::class, 'unfavorite'])->name('restaurants.unfavorite');
+
+Route::controller(UserController::class)->group(function () {
+  Route::get('users/mypage', 'mypage')->name('mypage');
+  Route::get('users/mypage/edit', 'edit')->name('mypage.edit');
+  Route::put('users/mypage', 'update')->name('mypage.update');
+  Route::get('users/mypage/favorite', 'favorite')->name('mypage.favorite');
+  Route::delete('users/mypage/delete', 'destroy')->name('mypage.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
+
+Route::controller(CheckoutController::class)->group(function () {
+  Route::get('checkout', 'index')->name('checkout.index');
+  Route::post('checkout', 'store')->name('checkout.store');
+  Route::get('checkout/success', 'success')->name('checkout.success');
+});
+
